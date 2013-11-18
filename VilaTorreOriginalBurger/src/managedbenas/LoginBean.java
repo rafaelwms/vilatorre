241b635d97884989dd4756edd904ff4c5c10c2a6
rafael.wmsa <rafael.wmsa@3e72150a-6c23-783d-c3fc-65a71ff6796e>
2013-11-18 02:30:41 +0000

    mais testes

---
+++ VilaTorreOriginalBurger/src/managedbenas/LoginBean.java
@@ -0,0 +1,46 @@
+package managedbenas;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import basicasHumana.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+	
+	private String login;
+	private String senha;
+	private Usuario usuarioLogado;
+	
+	public String tentarLogar(){
+		
+		if(login == "rafael" && senha == "wms") {
+			
+			return "/loginSucedido.xhtml?faces-redirect=true";
+		}else
+		
+		
+		return "";
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Usuario getUsuarioLogado() {
+		return usuarioLogado;
+	}
+	public void setUsuarioLogado(Usuario usuarioLogado) {
+		this.usuarioLogado = usuarioLogado;
+	}
+
+}
