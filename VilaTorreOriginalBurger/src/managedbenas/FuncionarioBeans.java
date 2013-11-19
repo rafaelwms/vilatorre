21d3f64af4da5beda1175176a565a07cce02861c
rafael.wmsa@gmail.com <rafael.wmsa@gmail.com@3e72150a-6c23-783d-c3fc-65a71ff6796e>
2013-11-19 03:26:43 +0000

    Novidades ótimas!

---
+++ VilaTorreOriginalBurger/src/managedbenas/FuncionarioBeans.java
@@ -0,0 +1,12 @@
+package managedbenas;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class FuncionarioBeans {
+	
+	
+
+}
---
+++ VilaTorreOriginalBurger/src/negocio/NegocioComum.java
@@ -0,0 +1,30 @@
+package negocio;
+
+public class NegocioComum {
+
+	public boolean validarString(String texto, String objeto, String atributo,
+			int de, int ate, String artigoObjeto, String artigoAtributo)
+			throws Exception {
+		if (de == ate) {
+
+			if (texto.trim().length() != ate) {
+
+				throw new Exception(artigoAtributo.toUpperCase() + " "
+						+ atributo + " d" + artigoObjeto.toLowerCase() + " "
+						+ objeto + " deve conter " + de + " caracteres.");
+			}
+
+		} else {
+
+			if ((texto.trim().length() < de) || (texto.trim().length() > ate)) {
+
+				throw new Exception(artigoAtributo.toUpperCase() + " "
+						+ atributo + " d" + artigoObjeto.toLowerCase() + " "
+						+ objeto + " deve conter de " + de + " a " + ate
+						+ " caracteres.");
+			}
+		}
+		return true;
+	}
+
+}
