5cf78ba8f3037b611f395c1c84d3561db5643b37
rafael.wmsa <rafael.wmsa@3e72150a-6c23-783d-c3fc-65a71ff6796e>
2013-12-03 03:37:27 +0000

    need some fixes... and very well

--- VilaTorre/WebContent/script/money.js
+++ VilaTorre/WebContent/script/money.js
@@ -18,3 +18,12 @@ function moeda(v){
     v=v.replace(/(\d{1})(\d{1,2})$/,"$1,$2") // coloca virgula antes dos ultimos 2 digitos   
     return v;   
 } 
+
+function execmascara(){   
+    v_obj.value=v_fun(v_obj.value)   
+}   
+  
+function numeros(v){   
+    v=v.replace(/\D/g,""); // permite digitar apenas numero     
+    return v;   
+} 
